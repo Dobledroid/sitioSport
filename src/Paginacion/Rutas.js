2fd40f19899e8a2832e528bed45b46efb0568563
OsYer <20210720@uthh.edu.mx>
2024-07-25 09:30:00 -0600

    Tu mensaje de commit describiendo los cambios

--- src/Paginacion/Rutas.js
+++ src/Paginacion/Rutas.js
@@ -46,7 +46,8 @@ import EditarProducto from './Administracion/Productos/EditarProducto';
 // import Header2 from '../Esquema/Headerq';
 // import Bodys from '../Esquema/Bodys';
 
-
+import ComprasAdm1 from './Administracion/Compras';
+import Detalleventa from './Administracion/Detalleventa';
 import ApiDataDisplay from './Administracion/Usuarios/ApiDataDisplay';
 
 import Error404 from './Validaciones/Error404/Error404';
@@ -133,7 +134,9 @@ const Rutas = () => {
           <Route path='/historialMembresias' element={<HistorialMembresias />}></Route>
 
           <Route path='/mis-compras' element={<Compras />}></Route>
-
+          <Route path='/compras' element={<ComprasAdm1 />}></Route>
+          <Route path='/detalleventa' element={<Detalleventa />}></Route>
+          
           <Route path='/profile' element={<Profile />}></Route>
           <Route path='/detalle-compra/:ID_pedido' element={<DetalleCompra />}></Route>
         
