4b1a5ef9b367a979e97f957f7254830d7836f367
OsYer <20210720@uthh.edu.mx>
2024-11-04 00:36:25 -0600

    Porductos sin conexion, revisar funcionamiento raro de Sidebar

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+// babel.config.js
+module.exports = {
+    presets: [
+      '@babel/preset-env',         // Para soporte de sintaxis moderna de ECMAScript
+      '@babel/preset-react',       // Para JSX (React)
+      '@babel/preset-typescript'   // Si estás usando TypeScript
+    ],
+  };
+  
