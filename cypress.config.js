d533cba4adb28f8ccc8d407938e3f5481bf21073
OsYer <20210720@uthh.edu.mx>
2024-11-06 16:02:40 -0600

    merge master y develop

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // Implementa los controladores de eventos de Node aquí
     },
-    defaultCommandTimeout: 300000, // Tiempo de espera predeterminado para los comandos en ms
-    pageLoadTimeout: 300000,       // Tiempo de espera para la carga de página en ms
-    video: false                   // Desactiva la grabación de video, si lo prefieres
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Define el patrón de archivos de prueba
+    defaultCommandTimeout: 300000,
+    pageLoadTimeout: 300000,
+    video: false
   },
 });
