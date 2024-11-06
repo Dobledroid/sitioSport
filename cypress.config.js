381fddb38a450dc1361d1c8622326d64f7157fc4
OsYer <20210720@uthh.edu.mx>
2024-11-06 02:05:44 -0600

    Jest test

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implementa los controladores de eventos de Node aquí
     },
+    defaultCommandTimeout: 300000, // Tiempo de espera predeterminado para los comandos en ms
+    pageLoadTimeout: 300000,       // Tiempo de espera para la carga de página en ms
+    video: false                   // Desactiva la grabación de video, si lo prefieres
   },
 });
