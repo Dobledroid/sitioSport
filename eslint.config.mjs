3040e62cd25e1eff821be719a82a8a3ee7882123
Dobledroid <carlosexequiel360@gmail.com>
2024-11-17 21:21:09 -0600

    modificación en body y versión de react de eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect" // Esto le indica a ESLint que detecte automáticamente la versión de React
+      }
+    }
+  }
 ];
