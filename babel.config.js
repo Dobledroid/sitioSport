b7a61d1d28580bcafeb9dc41399b5b57f144c74c
OsYer <carlosexequiel360@gmail.com>
2024-11-24 01:59:37 -0600

    PWA CI CD 4.4

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 // babel.config.js
 
-  module.exports = {
-    plugins: ['@babel/plugin-proposal-private-property-in-object'],    
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript',
-      'react-app',
-    ],
-  };
-  
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+    'react-app',
+  ], 
+  plugins: ['@babel/plugin-proposal-private-property-in-object'],
+
+};
