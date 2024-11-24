d0574e1f02ad25a69bf92d3398af358504714b05
OsYer <carlosexequiel360@gmail.com>
2024-11-24 01:47:16 -0600

    PWA CI DI 4.2

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 // babel.config.js
-module.exports = {    
-  plugins: ['babel-plugin-react-html-attrs'],
+
+  module.exports = {    
     presets: [
-      '@babel/preset-env',         // Para soporte de sintaxis moderna de ECMAScript
-      '@babel/preset-react',       // Para JSX (React)
-      '@babel/preset-typescript'   // Si estás usando TypeScript
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript'
     ],
   };
+  
